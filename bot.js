7eecf7240389bac5292f07fdc16b314c7e50f82e
Kiwi <kio.thedev@gmail.com>
2019-10-11 02:39:55 -0400

    Removed Stagnent code
    Replaced Stagnent code with some good ol'fashion NULL
    
    -Changed Kiru's presence to permanantly be Listening "To the community".
    -Removed Node-Cron
    -Added README

--- bot.js
+++ bot.js
@@ -1,65 +1,39 @@
-const Enmap = require("enmap")
-const cron = require("node-cron")
-const discord = require("discord.js")
-const ConfigGrab = require("node-json-config")
-const bannedEvents = ["webserver.js", "api.js", "extrafunctions.js"]
-const fs = require("fs")
-const path = require("path")
+const Enmap = require('enmap')
+const discord = require('discord.js')
+const ConfigGrab = require('node-json-config')
+const bannedEvents = ['webserver.js', 'api.js', 'extrafunctions.js']
+const fs = require('fs')
+const path = require('path')
 
-const triggerNewPresence = () => {
-	client.data.save()
-	client.params.save()
-	if (client.data.get("presence").length === 0) {
-		client.user
-			.setPresence({
-				game: {
-					type: "LISTENING",
-					name: "the community!"
-				},
-				status: "online"
-			})
-			.then(() => {
-				client.log("I changed my presence!")
-			})
-	} else {
-		client.user
-			.setPresence({
-				game: {
-					name: client.data.get("presence")[
-						Math.floor(
-							Math.random() * client.data.get("presence").length
-						)
-					]
-				}
-			})
-			.then(() => {
-				client.log("I changed my presence!")
-			})
-	}
-	client.data.put("presence", [])
-}
+client.user.setPresence({
+	game: {
+		type: 'LISTENING',
+		name: 'the community!'
+	},
+	status: 'online'
+})
 
 const msg = message => {
-	client.channels.get(client.params.get("channels.genericLogs")).send(message)
+	client.channels.get(client.params.get('channels.genericLogs')).send(message)
 	console.log(message)
 }
 
-process.on("uncaughtException", err => {
+process.on('uncaughtException', err => {
 	console.log("There's an issue in one of your modules. " + err.stack)
 })
 
 const client = new discord.Client()
-client.data = new ConfigGrab(path.join(__dirname, "./config/data.json"))
-client.params = new ConfigGrab(path.join(__dirname, "./config/config.json"))
+client.data = new ConfigGrab(path.join(__dirname, './config/data.json'))
+client.params = new ConfigGrab(path.join(__dirname, './config/config.json'))
 client.extraFunction = require(path.join(
 	__dirname,
-	"./events/extrafunctions.js"
+	'./events/extrafunctions.js'
 ))
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir('./events/', (err, files) => {
 	if (err) return console.error(err.stack)
 	files.forEach(file => {
-		if (!bannedEvents.includes(file) && file.endsWith(".js")) {
+		if (!bannedEvents.includes(file) && file.endsWith('.js')) {
 			let event = null
 			try {
 				event = require(path.join(__dirname, `./events/${file}`))
@@ -70,7 +44,7 @@ fs.readdir("./events/", (err, files) => {
 				)
 				process.exit(1)
 			}
-			const eventName = file.split(".")[0]
+			const eventName = file.split('.')[0]
 			client.on(eventName, event.bind(null, client))
 		}
 	})
@@ -78,10 +52,10 @@ fs.readdir("./events/", (err, files) => {
 
 const addCommands = () => {
 	client.commands = new Enmap()
-	fs.readdir(path.join(__dirname, "./commands/"), (err, files) => {
+	fs.readdir(path.join(__dirname, './commands/'), (err, files) => {
 		if (err) return console.error(err.stack)
 		files.forEach(file => {
-			if (!file.endsWith(".js")) return
+			if (!file.endsWith('.js')) return
 			let quip = false
 			let props = null
 			try {
@@ -94,8 +68,8 @@ const addCommands = () => {
 				quip = true
 			}
 			if (!quip) {
-				if (Object.keys(props).includes("run")) {
-					const commandName = file.split(".")[0]
+				if (Object.keys(props).includes('run')) {
+					const commandName = file.split('.')[0]
 					if (props.load !== undefined) props.load(client)
 					client.commands.set(commandName, props)
 				} else {
@@ -111,7 +85,7 @@ const addCommands = () => {
 				}
 			}
 		})
-		console.log("All commands are loaded!")
+		console.log('All commands are loaded!')
 	})
 }
 
@@ -120,29 +94,24 @@ client.reloadAll = () => {
 		delete require.cache[key]
 	})
 	addCommands()
-	client.log("Reloaded all commands.")
+	client.log('Reloaded all commands.')
 }
 
-client.on("ready", () => {
+client.on('ready', () => {
 	addCommands()
 	client.log = msg
-	client.log("===================================================")
+	client.log('===================================================')
 	client.log(
-		`Logged in as ${client.user.tag} on ` + new Date().toUTCString() + "!"
+		`Logged in as ${client.user.tag} on ` + new Date().toUTCString() + '!'
 	)
-	if (!fs.existsSync(path.join(__dirname, "./commands/core.js"))) {
+	if (!fs.existsSync(path.join(__dirname, './commands/core.js'))) {
 		console.log(
-			"===================================================\n" +
-				"WARNING!\nThis version of Kiru will be unsupported by Team Kiru!\n" +
-				"Core.js is required for proper operation.\n" +
-				"==================================================="
+			'===================================================\n' +
+				'WARNING!\nThis version of Kiru will be unsupported by Team Kiru!\n' +
+				'Core.js is required for proper operation.\n' +
+				'==================================================='
 		)
 	}
-
-	cron.schedule("*/30 * * * *", () => {
-		triggerNewPresence()
-	})
-	triggerNewPresence()
 })
 
-client.login(client.params.get("token"))
+client.login(client.params.get('token'))
